51faab5d692ca9b83c1d0bd3ddaebd4b264e3315
Sarah Currey <curreysarah28@gmail.com>
2018-03-02 21:45:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-function roughScale(1, 80) {
-  var parsed = parseInt(x, base);
+function add(1, 80) {
+  var parsed = parseInt(1, 80);
   return parsed * 81;
 }
 
-console.log(add ('80', 1));
+console.log(add ('1', 80));
 // expected output: 81
 
  
