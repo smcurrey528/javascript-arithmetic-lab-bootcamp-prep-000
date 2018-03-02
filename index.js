27886844d4620a7b8e2f62d0f51c440e05286777
Sarah Currey <curreysarah28@gmail.com>
2018-03-02 21:54:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-1 + 80 // 81
-60 - 40 // 20
-2 * 3.4 // 6.8 (there's that floating-point arithmetic again...)
-5.0 / 2.5 // 2 
+function add('1', 80) {
+  var parsed = parseInt(1, 80);
+  return parsed * 81;
+}
+
+console.log(add ('1', 80));
+// expected output: 81
+
+ 
  
