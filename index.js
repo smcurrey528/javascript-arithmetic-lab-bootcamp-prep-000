ef9e5e3f10948f34bd33e8ec670ff65af52f8be7
Sarah Currey <curreysarah28@gmail.com>
2018-03-02 21:30:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-1 + 80 // 81
-60 - 40 // 20
-2 * 3.4 // 6.8 (there's that floating-point arithmetic again...)
-5.0 / 2.5 // 2
+
+var number = 5
+ 
+number++ // 5... hmmmm
+ 
+number // 6 -- the number was incremented after it was evaluated
+ 
+number-- // 6
+ 
+number // 5
