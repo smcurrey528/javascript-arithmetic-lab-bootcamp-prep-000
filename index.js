ca35ff921b63dfd1d58102b120c570290930c28d
Sarah Currey <curreysarah28@gmail.com>
2018-03-02 21:44:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,33 +1,10 @@
- var number = 10
- 
- function add5() {
-   number += 5
- }
- 
- function divideBy3() {
-   number /=3
- }
- 
- divideBy3()
- 
- console.log(number) // 3.3333335 
- 
- add5()
- console.log(number) // 8.33333335 
- 
- // reset number 
- number=10
- 
- add5 ()
- console.log(number) // 15
- 
-divideBy3()
- 
-console.log(number) // 5
+function roughScale(1, 80) {
+  var parsed = parseInt(x, base);
+  return parsed * 81;
+}
 
-parseInt ('2.2222', 10)
-
-
+console.log(add ('80', 1));
+// expected output: 81
 
  
  
